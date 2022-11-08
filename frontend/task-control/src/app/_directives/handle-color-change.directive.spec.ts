0e81de43564811b2fbb4fbc4fb7b3c3ece32f1bf
DmytroTarasov <dtarasov24s@gmail.com>
2022-11-09 01:56:36 +0200

    test: create tests for handlecolorchange directive

---
+++ frontend/task-control/src/app/_directives/handle-color-change.directive.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+
+import { HandleColorChangeDirective } from './handle-color-change.directive';
+import { AppState } from '../store/app.reducer';
+import * as BoardActions from '../boards/store/board.actions';
+
+@Component({
+  template: `
+    <input type="color" ng-reflect-name="colorType1" value="#000000" appHandleColorChange>
+  `
+})
+class TestColorChangeComponent {}
+
+describe('HandleColorChange Directive', () => {
+  let fixture: ComponentFixture<TestColorChangeComponent>;
+  let inputElement: DebugElement;
+  let store: MockStore<AppState>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideMockStore()],
+      declarations: [TestColorChangeComponent, HandleColorChangeDirective],
+    });
+
+    fixture = TestBed.createComponent(TestColorChangeComponent);
+    store = TestBed.inject(MockStore);
+
+    inputElement = fixture.debugElement.query(By.css('input'));
+
+    fixture.detectChanges();
+    spyOn(store, 'dispatch').and.callFake(() => {});
+  });
+
+  it('should handle focusout event', waitForAsync(() => {
+    inputElement.nativeElement.dispatchEvent(new Event('focusout'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(
+        BoardActions.setColumnColor({ colorType: 'colorType1', color: '#000000' })
+      );
+    });
+  }));
+});
