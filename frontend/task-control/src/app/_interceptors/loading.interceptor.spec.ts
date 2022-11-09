10ab9a52f7e3728fe06bd877252cf8314e5f4fcb
DmytroTarasov <dtarasov24s@gmail.com>
2022-11-09 12:25:43 +0200

    test: create tests for loading interceptor & refactor tests for auth service

---
+++ frontend/task-control/src/app/_interceptors/loading.interceptor.spec.ts
@@ -0,0 +1,72 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { MemoizedSelector } from '@ngrx/store';
+
+import { environment } from 'src/environments/environment';
+
+import { LoadingInterceptor } from './loading.interceptor';
+import { AppState } from '../store/app.reducer';
+import * as fromLoading from '../shared/loader/store/loading.reducer';
+import * as LoadingActions from '../shared/loader/store/loading.actions';
+import { getLoading } from '../shared/loader/store/loading.selectors';
+import { BoardsService } from '../_services/boards.service';
+import { finalize } from 'rxjs';
+
+describe('Loading Interceptor', () => {
+  let httpMock: HttpTestingController;
+  let store: MockStore<AppState>;
+  let boardsService: BoardsService;
+  let mockLoadingSelector: MemoizedSelector<fromLoading.State, boolean>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        BoardsService,
+        provideMockStore(),
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: LoadingInterceptor,
+          multi: true
+        }
+      ]
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
+    store = TestBed.inject(MockStore);
+    boardsService = TestBed.inject(BoardsService);
+
+    mockLoadingSelector = store.overrideSelector(getLoading, false);
+    spyOn(store, 'dispatch').and.callFake(() => {});
+  });
+
+  afterEach(() => {
+    store.complete();
+  });
+
+  afterAll(() => {
+    httpMock.verify();
+  });
+
+it('should set loading to true when the request starts and to false when it ends', waitForAsync(() => {
+    boardsService
+      .getBoards()
+      .pipe(
+        finalize(() => {
+          expect(store.dispatch).toHaveBeenCalledTimes(2);
+          expect(store.dispatch).toHaveBeenCalledWith(LoadingActions.completeLoading());
+        })
+      )
+      .subscribe((res) => {
+        expect(store.dispatch).toHaveBeenCalledTimes(1)
+        expect(store.dispatch).toHaveBeenCalledWith(LoadingActions.startLoading());
+      });
+
+    httpMock.expectOne(`${environment.serverUrl}/boards`).flush([]);
+  }));
+});
--- frontend/task-control/src/app/_services/auth.service.spec.ts
+++ frontend/task-control/src/app/_services/auth.service.spec.ts
@@ -3,98 +3,77 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { User } from '../_models/user.model';
 import { environment } from 'src/environments/environment';
 
 describe('Auth Service', () => {
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
-  let authService;
+  let httpMock: HttpTestingController;
+  let authService: AuthService;
   const user = new User('bob@test.com', 'Bob', '2022-11-06', 'token');
-  const messageSuccess = 'Profile was created successfully';
-  const messageFailure = 'Password length must be at least 6 characters long';
+  const resSuccess = { message: 'Profile was created successfully' };
+  const resFailure = { message: 'Password length must be at least 6 characters long' };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [AuthService],
+      providers: [AuthService]
     });
 
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
+    httpMock = TestBed.inject(HttpTestingController);
     authService = TestBed.inject(AuthService);
   });
 
   afterEach(() => {
-    httpTestingController.verify();
+    httpMock.verify();
   });
 
   describe('login', () => {
     it('should login the user', () => {
-      let actualUser: User;
       authService
         .login(user.email, '111111')
-        .subscribe((user: User) => (actualUser = user));
+        .subscribe(user => expect(user).toEqual(user));
 
-      const req = httpTestingController.expectOne(
-        `${environment.serverUrl}/auth/login`
-      );
+      const req = httpMock.expectOne(`${environment.serverUrl}/auth/login`);
       req.flush(user);
-
-      expect(actualUser).toEqual(user);
     });
 
     it('should not login the user', () => {
       const errorEvent = new ErrorEvent('Invalid creds');
       const status = 400;
       const statusText = 'Bad request';
-      let actualError;
       authService
         .login(user.email, '1111')
         .subscribe({
-          error: (error) => (actualError = error)
+          error: (error) => {
+            expect(error.error).toEqual(errorEvent);
+            expect(error.status).toEqual(status);
+            expect(error.statusText).toEqual(statusText);
+          }
         });
 
-      const req = httpTestingController
-        .expectOne(`${environment.serverUrl}/auth/login`);
-
+      const req = httpMock.expectOne(`${environment.serverUrl}/auth/login`);
       req.flush(errorEvent, { status, statusText });
-
-      expect(actualError.error).toEqual(errorEvent);
-      expect(actualError.status).toEqual(status);
-      expect(actualError.statusText).toEqual(statusText);
     });
   });
 
   describe('signup', () => {
     it('should register the user', () => {
-      let actualMessage: string;
       authService
         .signup(user.email, user.username, '111111')
-        .subscribe((message) => (actualMessage = message));
+        .subscribe((res) => expect(res).toEqual(resSuccess));
 
-      const req = httpTestingController.expectOne(
-        `${environment.serverUrl}/auth/register`
-      );
-      req.flush(messageSuccess);
-
-      expect(actualMessage).toEqual(messageSuccess);
+      const req = httpMock.expectOne(`${environment.serverUrl}/auth/register`);
+      req.flush(resSuccess);
     });
 
     it('should not register the user', () => {
-      let actualMessage: string;
       authService
         .signup(user.email, user.username, '111111')
-        .subscribe((message) => (actualMessage = message));
+        .subscribe((res) => expect(res).toEqual(resFailure));
 
-      const req = httpTestingController.expectOne(
-        `${environment.serverUrl}/auth/register`
-      );
-      req.flush(messageFailure);
-
-      expect(actualMessage).toEqual(messageFailure);
+      const req = httpMock.expectOne(`${environment.serverUrl}/auth/register`);
+      req.flush(resFailure);
     });
   });
 
